881e60760fe0311f04e6adf871c62dbcd59c3e12
magnu <2magnu@gmail.com>
2023-10-23 09:56:43 +0300

    remove modals templates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const modalTriggers = document.querySelectorAll('.js-open-modal');
-    const modalStack = [];
+$(document).ready(function(){
 
-    function openModal(modalId) {
-        const template = document.getElementById(modalId);
-
-        if (template) {
-            const modalContent = document.importNode(template.content, true);
-            const modalInner = document.querySelector('.modal__inner');
-
-            if (modalInner) {
-                modalInner.innerHTML = '';
-                modalInner.appendChild(modalContent);
-
-                const inputsWithMasks = modalInner.querySelectorAll('input[data-mask]');
-                inputsWithMasks.forEach(input => {
-                    const inputMaskOptions = JSON.parse(input.getAttribute('data-mask'));
-                    Inputmask(inputMaskOptions).mask(input);
-                });
-
-                const modalWrapper = document.querySelector('.modal__wrapper');
-                modalWrapper.style.display = 'block';
-                document.body.style.overflow = 'hidden';
-
-                const closeButton = document.querySelector('.modal__close');
-
-                closeButton.addEventListener('click', function() {
-                    modalStack.pop(); // Убираем последний элемент из стека
-                    const previousModalId = modalStack[modalStack.length - 1]; // Получаем предыдущий modalId из стека
-                    if (previousModalId) {
-                        modalStack.pop();
-                        openModal(previousModalId); // Открываем предыдущее модальное окно из стека
-                    } else {
-                        modalWrapper.style.display = 'none';
-                        document.body.style.overflow = 'auto';
-                    }
-                });
-
-                modalWrapper.addEventListener('click', function(event) {
-                    if (event.target === modalWrapper && event.target !== modalInner) {
-                        closeButton.click();
-                    }
-                });
-
-                modalStack.push(modalId); // Добавляем текущий modalId в стек
-
-                const nestedModalTriggers = document.querySelectorAll('.js-open-modal');
-                nestedModalTriggers.forEach(trigger => {
-                    trigger.addEventListener('click', function() {
-                        const nestedModalId = this.getAttribute('data-modal');
-                        modalStack.push(modalId); // Добавляем текущий modalId в стек перед открытием нового окна
-                        openModal(nestedModalId);
-                    });
-                });
-            } else {
-                console.error('Модальное окно не найдено в DOM');
+    $('#phone').mask('+7 (ZZZ) ZZZ-ZZ-ZZ', {
+        translation: {
+            'Z': {
+                pattern: /[0-9]/, optional: true
             }
-        } else {
-            console.error('Шаблон модального окна не найден');
-        }
-    }
+        },
+        placeholder: "+7(___) ___ - __ - __"
+    });
 
-    modalTriggers.forEach(trigger => {
-        trigger.addEventListener('click', function() {
-            const modalId = this.getAttribute('data-modal');
-            modalStack.push(modalId);
-            openModal(modalId);
-        });
+    let currentModal = null;
+    let previousModal = null;
+
+    document.addEventListener('click', function (event) {
+        const target = event.target;
+
+        if (target.classList.contains('js-open-modal')) {
+            const modalId = target.getAttribute('data-modal');
+            previousModal = currentModal;
+            currentModal = document.getElementById(modalId);
+
+            if (previousModal) {
+                previousModal.style.display = 'none';
+            }
+
+            currentModal.style.display = 'block';
+            document.querySelector('.modal__wrapper').style.display = 'block';
+        }
+
+        if (target.classList.contains('modal__close') || target.classList.contains('modal__wrapper')) {
+            if (previousModal) {
+                currentModal.style.display = 'none';
+                previousModal.style.display = 'block';
+                currentModal = previousModal;
+                previousModal = null;
+            } else {
+                document.querySelector('.modal__wrapper').style.display = 'none';
+                currentModal.style.display = 'none';
+                currentModal = null;
+            }
+        }
     });
 });
